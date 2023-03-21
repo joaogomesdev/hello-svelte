8addfede1cdff6af5fb30865cb0a9478a2d43ebc
João Gomes <joaogomes@significa.co>
2023-03-21 16:48:18 +0000

    Setup project and prisma seed

---
+++ svelte-kit/svelte-kit-ssr/prisma/seed.ts
@@ -0,0 +1,36 @@
+import { prismaClient } from '../src/lib/prisma'
+
+type Post = {
+	title: string
+	body: string
+}
+
+async function getPosts() {
+	const response = await fetch('https://dummyjson.com/posts')
+
+	const { posts } = await response.json()
+	return posts as Post[]
+}
+
+function slugify(text: string) {
+	return text
+		.replace(/\s/g, '-')
+		.replace(/[^a-zA-Z0-9-]/g, '')
+		.toLowerCase()
+}
+
+async function main() {
+	const posts = await getPosts()
+
+	for (const post of posts) {
+		await prismaClient.post.create({
+			data: {
+				title: post.title,
+				content: post.body,
+				slug: slugify(post.title)
+			}
+		})
+	}
+}
+
+main()
