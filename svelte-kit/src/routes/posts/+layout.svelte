915c53f0b4dc6265d0fc250352d1a5d315d3792b
João Gomes <joaogomes@significa.co>
2023-03-21 15:56:49 +0000

    Learn about +layout

---
+++ svelte-kit/src/routes/posts/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  
+</script>
+
+
+<div class="posts">
+  <aside>
+    <h4>Your posts</h4>
+
+    <nav>
+      <ul>
+        <li>
+          <a href="/posts/one">Post one</a>
+        </li>
+        <li>
+          <a href="/posts/two">Post two</a>
+        </li>
+        <li>
+          <a href="/posts/three">Post three</a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+  
+  <main>
+    <slot /> 
+  </main>
+  
+</div>
+
+<style>
+  .posts {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 200px 60ch;
+    margin-top: 2rem;
+  }
+</style>
