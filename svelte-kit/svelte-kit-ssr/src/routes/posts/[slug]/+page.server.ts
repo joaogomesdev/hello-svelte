f57a1900d49c6648914baa7084d79c79c1a1272b
João Gomes <joaogomes@significa.co>
2023-03-22 16:15:38 +0000

    Running only on SRR

---
+++ svelte-kit/svelte-kit-ssr/src/routes/posts/[slug]/+page.server.ts
@@ -0,0 +1,20 @@
+import { error } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+import { prismaClient } from '$lib/prisma'
+
+export const load: PageServerLoad = async ({ params }) => {
+	console.log(params)
+
+	const post = await prismaClient.post.findUnique({
+		where: {
+			slug: params.slug
+		}
+	})
+
+	if (!post) {
+		throw error(404, 'Post not found')
+	}
+
+	return { post }
+}
