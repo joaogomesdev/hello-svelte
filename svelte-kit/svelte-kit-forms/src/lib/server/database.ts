9f66ae0208c33c9c27c5d56c5929564cfd216ed3
João Gomes <joaogomes@significa.co>
2023-03-23 16:16:52 +0000

    Create fake database

---
+++ svelte-kit/svelte-kit-forms/src/lib/server/database.ts
@@ -0,0 +1,24 @@
+type Todo = { id: number; text: string; completed: boolean };
+
+let todos: Todo[] = [{ id: Date.now(), text: 'Todo', completed: false }];
+
+export function getTodos() {
+	return todos;
+}
+
+export function addTodo(text: string) {
+	const todo: Todo = {
+		id: Date.now(),
+		text,
+		completed: false
+	};
+	todos.push(todo);
+}
+
+export function removeTodo(id: number) {
+	todos = todos.filter((todo) => todo.id == id);
+}
+
+export function clearTodos() {
+	todos = [];
+}
---
+++ svelte-kit/svelte-kit-forms/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="content">
+	<slot />
+</div>
+
+<style>
+	.content {
+		margin-top: 2rem;
+	}
+</style>
