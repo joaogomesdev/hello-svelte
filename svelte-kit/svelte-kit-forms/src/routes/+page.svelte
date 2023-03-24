5f6a24da7751e850ec55b40393cbe08126bbc5d3
João Gomes <joaogomes@significa.co>
2023-03-24 15:43:11 +0000

    more form enhance things

--- svelte-kit/svelte-kit-forms/src/routes/+page.svelte
+++ svelte-kit/svelte-kit-forms/src/routes/+page.svelte
@@ -1,1 +1,35 @@
-<h1>Home</h1>
+<script lang="ts">
+	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+
+	const login: SubmitFunction = () => {
+		return async ({ result }) => {
+			if (result.type === 'redirect') {
+				await applyAction(result);
+			}
+		};
+	};
+</script>
+
+<h1>Login</h1>
+
+<form method="POST" action="/login" use:enhance={login}>
+	<input type="text" name="user" value={form?.data.user ?? ''} />
+	{#if form?.errors?.user}
+		<p class="error">Username is required!</p>
+	{/if}
+	<input type="password" name="password" value={form?.data.password ?? ''} />
+	{#if form?.errors?.password}
+		<p class="error">Password is required!</p>
+	{/if}
+
+	<button type="submit">Login</button>
+</form>
+
+<style>
+	.error {
+		color: tomato;
+	}
+</style>
