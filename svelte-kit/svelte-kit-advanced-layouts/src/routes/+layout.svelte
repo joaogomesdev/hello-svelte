45446dc855cfff24a2387fc78e27f40c2e28e287
João Gomes <joaogomes@significa.co>
2023-03-24 16:11:44 +0000

    group layouts

---
+++ svelte-kit/svelte-kit-advanced-layouts/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { DevTools } from '$lib/devtools'
+
+	import '@picocss/pico'
+	import '../app.css'
+</script>
+
+<DevTools />
+
+<slot />
+
+<svelte:head>
+	<link rel="icon" href="https://fav.farm/🪶" />
+	<title>Plume</title>
+</svelte:head>
